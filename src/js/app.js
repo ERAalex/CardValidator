2aaf87f8b22736174c15de55ef13736f912b7a08
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2023-12-23 16:58:09 +0300

    First commit

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+import { greet } from './module'
+
+// TODO: write code here
+
+// comment this to pass build
+const unusedVariable = "variable";
+
+// for demonstration purpose only
+export default function demo(value) {
+  return `Demo: ${value}`;
+}
+
+greet('James')
+console.log("app.js included");
