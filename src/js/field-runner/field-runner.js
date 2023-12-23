4baf152f5d81f116280f2b040a147863d3f8977e
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2023-12-24 02:10:32 +0300

    feat/dont-repeat the same position

--- src/js/field-runner/field-runner.js
+++ src/js/field-runner/field-runner.js
@@ -2,11 +2,18 @@
 export default class ImageWidget {
   constructor(element) {
     this._element = element;
-    this._total_fields = 16;
+    this._total_fields = 4;
+    this._last_position = 0;
   }
 
   getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+    let choiceNumber = Math.floor(Math.random() * max);
+    if (choiceNumber === this._last_position) {
+      console.log('the same number')
+      this.getRandomInt(max)
+    }
+    this._last_position = choiceNumber
+    return choiceNumber
   }
 
   deleteActiveRabbit() {
@@ -18,21 +25,10 @@ export default class ImageWidget {
   }
 
   addRandomRabbit() {
-    // const activeElements = this._element.querySelectorAll('.item-table-active');
-
-    // if (activeElements.length != 0){
-    //   activeElements[0].classList.remove("item-table-active");
-    // }
-
     const newElements = this._element.querySelectorAll('.item-table');
-    
     let choiceField = this.getRandomInt(this._total_fields)
-
 
     let randomField = newElements[choiceField]
     randomField.classList.add("item-table-active");
-
   }
-
-
 }
