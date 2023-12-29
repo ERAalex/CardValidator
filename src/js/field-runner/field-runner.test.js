635f3853ee314fc8e90d4b9564a0960eec5b142c
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2023-12-29 16:02:22 +0300

    fix/Test error new Mock moments

--- src/js/field-runner/field-runner.test.js
+++ src/js/field-runner/field-runner.test.js
@@ -2,14 +2,47 @@ import ImageWidget from "./field-runner";
 
 describe("field-runner", () => {
   test("generateRandomNumber", () => {
+    // Let's mock some moments for our Rabbit programm
+    const mockQuerySelector = jest
+      .fn()
+      .mockReturnValue(document.createElement("div"));
+    document.querySelector = mockQuerySelector;
+
+    const mockQuerySelectorAll = jest
+      .fn()
+      .mockReturnValue([
+        document.createElement("span"),
+        document.createElement("span"),
+      ]);
+
     const widget = new ImageWidget(document.querySelector(".table-main"));
+    widget._element.querySelectorAll = mockQuerySelectorAll;
+
     const randomNumber = widget.getRandomInt(10);
     expect(randomNumber).toBeGreaterThanOrEqual(0);
     expect(randomNumber).toBeLessThanOrEqual(10);
+
+    // Restore the original function after the test
+    mockQuerySelector.mockRestore();
   });
 
   test("totalFields", () => {
+    // Let's mock some moments for our Rabbit programm
+    const mockQuerySelector = jest
+      .fn()
+      .mockReturnValue(document.createElement("div"));
+    document.querySelector = mockQuerySelector;
+
+    const mockQuerySelectorAll = jest
+      .fn()
+      .mockReturnValue([
+        document.createElement("span"),
+        document.createElement("span"),
+      ]);
+
     const widget = new ImageWidget(document.querySelector(".table-main"));
+    widget._element.querySelectorAll = mockQuerySelectorAll;
+
     expect(widget._total_fields).toBe(16);
   });
 });
