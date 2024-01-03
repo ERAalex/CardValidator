9fdc56f6f2b29b96f2d4a1bad2f0a27a4246d396
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2024-01-04 01:49:48 +0300

    fix/tests and linters

--- src/js/card-form/card-form.test.js
+++ src/js/card-form/card-form.test.js
@@ -9,13 +9,14 @@ describe("Card Form Widget functionality", () => {
     document.querySelector = mockQuerySelector;
 
     // create fake new input for class constructor and eventListener
-    const mainContainer = document.querySelector(".card-form")
-    const inputElent = mainContainer.appendChild(document.createElement('input'))
+    const mainContainer = document.querySelector(".card-form");
+    const inputElent = mainContainer.appendChild(
+      document.createElement("input")
+    );
     inputElent.className = "card-input";
 
     const widget = new CardForm(document.querySelector(".card-form"));
     widget._element.querySelector = mockQuerySelector;
-
 
     const totalMessages = widget._alertMessages;
     expect(totalMessages.length).toBe(3); // check all messages
@@ -33,32 +34,34 @@ describe("Check function checkTypeCard()", () => {
     document.querySelector = mockQuerySelector;
 
     // create fake new input for class constructor and eventListener
-    const mainContainer = document.querySelector(".card-form")
-    const inputElent = mainContainer.appendChild(document.createElement('input'))
+    const mainContainer = document.querySelector(".card-form");
+    const inputElent = mainContainer.appendChild(
+      document.createElement("input")
+    );
     inputElent.className = "card-input";
 
     const widget = new CardForm(document.querySelector(".card-form"));
     widget._element.querySelector = mockQuerySelector;
 
     // let check;
-    const card = []
+    const card = [];
 
-    card.push ({
+    card.push({
       visa: "4716020678046587",
       mir: "2216020678046587",
       mastercard: "5306951761201585",
       express: "342309932141239",
       discover: "6011197358921938",
       jsb: "3540137604271818",
-      diners: "36779869234598"
-    })
-    
+      diners: "36779869234598",
+    });
+
     // lets check all in cicle for each card
     for (const [key, value] of Object.entries(card[0])) {
-      const check = widget.checkTypeCard(value)
-      expect(check).toBe(key); 
+      const check = widget.checkTypeCard(value);
+      expect(check).toBe(key);
     }
-    
+
     mockQuerySelector.mockRestore();
   });
 });
--- src/js/card-types/card-types.test.js
+++ src/js/card-types/card-types.test.js
@@ -32,31 +32,12 @@ describe("Check function cardShow()", () => {
     // after call this function it will create 7 new elements in .cards-container (because we have 7 cards)
     widget.cardShow();
     const container = widget._element.querySelector(".cards-container");
-    expect(container.children.length).toBe(7); 
+    expect(container.children.length).toBe(7);
     mockQuerySelector.mockRestore();
   });
 });
 
-describe("Check function cardShow()", () => {
-  test("create new items by innerHTML", () => {
-    //
-    const mockQuerySelector = jest
-      .fn()
-      .mockReturnValue(document.createElement("div"));
-    document.querySelector = mockQuerySelector;
-
-    const widget = new CardWidget(document.querySelector(".cards-container"));
-    widget._element.querySelector = mockQuerySelector;
-
-    // after call this function it will create 7 new elements in .cards-container (because we have 7 cards)
-    widget.cardShow();
-    const container = widget._element.querySelector(".cards-container");
-    expect(container.children.length).toBe(7); 
-    mockQuerySelector.mockRestore();
-  });
-});
-
-describe("Check function cardShow()", () => {
+describe("Check function cardDeactivateAll()", () => {
   test("create new items by innerHTML", () => {
     //
     const mockQuerySelector = jest
@@ -72,19 +53,19 @@ describe("Check function cardShow()", () => {
       ]);
     document.querySelectorAll = mockQuerySelectorAll;
 
-
     const widget = new CardWidget(document.querySelector(".cards-container"));
     widget._element.querySelector = mockQuerySelector;
 
     // after call this function it will create 7 new elements in .cards-container (because we have 7 cards)
     widget.cardShow();
     const container = widget._element.querySelector(".cards-container");
-    expect(container.children.length).toBe(7); 
+    expect(container.children.length).toBe(7);
 
     widget.cardDeactivateAll();
 
-    const checkActivatedItems = widget._element.querySelectorAll(".filter-active");
-    console.log(checkActivatedItems)
+    const checkActivatedItems =
+      widget._element.querySelectorAll(".filter-active");
+    console.log(checkActivatedItems);
     expect(checkActivatedItems.length).toBe(7);
 
     mockQuerySelector.mockRestore();
